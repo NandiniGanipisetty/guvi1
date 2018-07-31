1a2a4bc2b42a7a0ea8c69bbe508e406d62d9f16c
NandiniGanipisetty <41725592+NandiniGanipisetty@users.noreply.github.com>
2018-07-31 15:42:55 +0530

    Update min.java

--- min.java
+++ min.java
@@ -13,11 +13,12 @@ class Ideone
 		int a[]=new int[10];
 		int n,i=0;
 		n=sc.nextInt();
+		System.out.println(""+n);
 		for( i=0;i<n;i++)
 		{
 			a[i]=sc.nextInt();
 			//System.out.println();
-			//	System.out.print(a[i]+"      ");
+				System.out.print(a[i]+"  ");
 		}
 		int max=a[0];
 		for(i=0;i<n;i++)
@@ -27,7 +28,8 @@ class Ideone
 				max=a[i];
 			}
 		}
-			System.out.println(max+"   ");
+			System.out.println();
+			System.out.print(max+"   ");
 		
 	}
 }
