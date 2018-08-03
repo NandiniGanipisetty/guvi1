d871d3a5c3d02d40c6f59143f3069899cb34c89c
NandiniGanipisetty <41725592+NandiniGanipisetty@users.noreply.github.com>
2018-08-03 16:00:25 +0530

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n1=0,n2=1,n3,count;
+		count=sc.nextInt();
+		for(int i=0;i<count-1;i++)
+		{
+			n3=n1+n2;
+			System.out.println(+n3);
+			n1=n2;
+			n2=n3;
+		}
+		
+		
+
+	}
+}
