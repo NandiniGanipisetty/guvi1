0e4308c407b5bdfc95ecfd6abe23559d5ae81cb7
NandiniGanipisetty <41725592+NandiniGanipisetty@users.noreply.github.com>
2018-08-04 14:18:40 +0530

    Update maximum.java

--- maximum.java
+++ maximum.java
@@ -9,16 +9,14 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-     int n;
+     int n=10;
      int c;
      Scanner s=new Scanner(System.in);
-     n=s.nextInt();
      ArrayList<Integer>a=new ArrayList<Integer>();
-     for(int i=0;i<n;i++){
+     for(int i=0;i<10;i++){
         c=s.nextInt();
         a.add(c);
      }
    System.out.print(Collections.max(a));
     }
 }
-
