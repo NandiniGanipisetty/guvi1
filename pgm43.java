914b183b4085e9f7c64fa905ee4d8d65104276d3
NandiniGanipisetty <41725592+NandiniGanipisetty@users.noreply.github.com>
2018-08-09 12:39:46 +0530

    Create pgm43.java

---
+++ pgm43.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s1;
+		String s2;
+		s1=sc.next();
+		s2=sc.next();
+		String s3=s1+s2;
+		System.out.println(s3);
+	}
+}
