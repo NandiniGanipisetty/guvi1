cfd55701dc71476c0cc2e62ff6a3ffb058004f1e
NandiniGanipisetty <41725592+NandiniGanipisetty@users.noreply.github.com>
2018-08-09 12:15:37 +0530

    Create repeat.java

---
+++ repeat.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s;
+		int n,i;
+		s=sc.next();
+	//	System.out.println("Input:"+s);
+		n=sc.nextInt();
+	//	System.out.println(n);
+		for(i=0;i<n;i++)
+		{
+			System.out.println(s);
+		}
+	}
+}
