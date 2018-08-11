acddc103fecbf2ed5b45bce6c2d18a9b51b1cee5
NandiniGanipisetty <41725592+NandiniGanipisetty@users.noreply.github.com>
2018-08-11 10:45:49 +0530

    Update pgm51.java

--- pgm51.java
+++ pgm51.java
@@ -7,7 +7,7 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
-	public static void main (String[] args) throws java.lang.Exception
+	public static void main (String[] args) throws Exception
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
