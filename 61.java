b93b8215247279ba1e3f30c5befea36eb46b7ac9
NandiniGanipisetty <41725592+NandiniGanipisetty@users.noreply.github.com>
2018-08-13 11:59:50 +0530

    Create 61.java

---
+++ 61.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Ideone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n;
+String str="";
+Scanner ja=new Scanner(System.in);
+str=ja.next();
+n=ja.nextInt();
+String a=str.substring(0,n);
+System.out.println(a);
+	}
+
+}
