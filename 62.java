9284fdab7593a2fa370623cf29eb361d4617fd0d
NandiniGanipisetty <41725592+NandiniGanipisetty@users.noreply.github.com>
2018-08-13 16:16:40 +0530

    Create 62.java

---
+++ 62.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+public static void main(String args[]){
+	String a;
+	Scanner in=new Scanner(System.in);
+	a=in.next();
+	String b=a.replaceAll("[^01]","");
+	if(b.equals(a)){
+		System.out.println("yes");
+	}
+	else{
+		System.out.println("no");
+	}
+	in.close();
+	}
+}
