b30aa371126dc94491469a3b6cc8bcd0d2502708
NandiniGanipisetty <41725592+NandiniGanipisetty@users.noreply.github.com>
2018-08-13 21:46:58 +0530

    Create 66.java

---
+++ 66.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,i=2;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+			if(n%i==0)
+			{
+
+				System.out.println("not prime");
+			}
+			else
+			{
+				System.out.println("prime");
+			}
+			
+		
+	}
+}
