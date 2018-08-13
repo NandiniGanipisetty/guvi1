19bef87bee53dff1ed9d217acd59c4db6132dd93
NandiniGanipisetty <41725592+NandiniGanipisetty@users.noreply.github.com>
2018-08-13 14:03:39 +0530

    Create 67.java

---
+++ 67.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Multiple10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,n,sum,sum1,a;
+Scanner ja=new Scanner(System.in);
+n=ja.nextInt();
+    a=n%10;
+	sum=10-a;
+	sum1=sum+n;
+System.out.println(sum1);
+	}
+
+}
