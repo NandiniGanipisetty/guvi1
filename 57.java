d879626468490c506c5b5a2db01035f2336ccc15
NandiniGanipisetty <41725592+NandiniGanipisetty@users.noreply.github.com>
2018-08-13 10:41:46 +0530

    Update 57.java

--- 57.java
+++ 57.java
@@ -4,7 +4,6 @@ class Countnum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int i,n,n1,count=0;
-System.out.println("enter the number");
 Scanner ja=new Scanner(System.in);
 n=ja.nextInt();
 n1=ja.nextInt();
