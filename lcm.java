2d6a31d5670817870f72e225a3ddacb3f0e3b592
NandiniGanipisetty <41725592+NandiniGanipisetty@users.noreply.github.com>
2018-08-14 16:48:39 +0530

    Create lcm.java

---
+++ lcm.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+ class LCM {
+    public static void main(String[] args) {
+
+        int n1 , n2 , lcm;
+        Scanner sc=new Scanner(System.in);
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+
+        // maximum number between n1 and n2 is stored in lcm
+        lcm = (n1 > n2) ? n1 : n2;
+
+        // Always true
+        while(true)
+        {
+            if( lcm % n1 == 0 && lcm % n2 == 0 )
+            {
+                System.out.printf("%d", lcm);
+                break;
+            }
+            ++lcm;
+        }
+    }
+}
