e6fa32031dc08c8dbc697ac0fc2e3ab2cb825d27
NandiniGanipisetty <41725592+NandiniGanipisetty@users.noreply.github.com>
2018-08-14 12:58:12 +0530

    Create 74.java

---
+++ 74.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+ class Ideone1{
+     public static void main(String aa[]){
+        float d;
+        Scanner s=new Scanner(System.in);
+        d=s.nextFloat();
+      int v=  Math.round(d);
+      System.out.println(v);
+  
+}
+    }
