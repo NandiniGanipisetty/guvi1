a369e90d3ed973194d66bb34943007a3b345615a
NandiniGanipisetty <41725592+NandiniGanipisetty@users.noreply.github.com>
2018-08-14 14:01:53 +0530

    Create 77.java

---
+++ 77.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+ class Ideone 
+ {
+	
+	public static void main(String[] args) 
+	{
+		int n, i;
+	Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		for(i=1;i<=n;i++)
+		{
+			if(n%i == 0) 
+			{
+				System.out.print(" "+i);
+			}
+		}
+	}
+}
