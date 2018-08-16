31583fc34b5b3eb341f5229bcb7cb027c036289b
NandiniGanipisetty <41725592+NandiniGanipisetty@users.noreply.github.com>
2018-08-16 11:25:57 +0530

    Create 84.java

---
+++ 84.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Ideone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,l;
+String str;
+String str1="",str2="";
+
+Scanner ja=new Scanner(System.in);
+str=ja.next();
+String s[]=str.split("");
+l=str.length();
+for(i=0;i<l;i++){
+if(i%2!=0)
+	str1=str1+s[i];
+else
+	str2=str2+s[i];
+}
+System.out.println(str2+" "+str1);
+}
+	}
